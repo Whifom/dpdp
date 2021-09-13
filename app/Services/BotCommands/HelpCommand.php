3517a25550b22cae7b312bae150d6ef131af9767
dn080684kvi <dd080684kvi@gmail.com>
2021-09-13 17:19:16 +0300

    настройка бота

--- app/Services/BotCommands/HelpCommand.php
+++ app/Services/BotCommands/HelpCommand.php
@@ -12,11 +12,6 @@ class HelpCommand extends Command
     protected $name = 'help';
 
     /**
-     * @var array Command Aliases
-     */
-    protected $aliases = ['listcommands'];
-
-    /**
      * @var string Command Description
      */
     protected $description = 'Help command, Get a list of commands';
